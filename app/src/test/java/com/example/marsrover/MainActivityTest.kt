56819c5163401b9615cd74ce5bf84c6e14cca609
avbolotova <direct.pavlova@yandex.ru>
2022-12-26 16:44:16 +0400

    add test

--- app/src/test/java/com/example/marsrover/MainActivityTest.kt
+++ app/src/test/java/com/example/marsrover/MainActivityTest.kt
@@ -13,6 +13,7 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.example.marsrover.view.main.MainAdapter
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -21,13 +22,30 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class MainActivityTest {
 
-    lateinit var navController: NavController
-
-
     @Before
     fun launchActivity() {
         ActivityScenario.launch(MainActivity::class.java)
     }
 
+    @Ignore("Need to find solution for CollapsingToolbarLayout with NestedScrollView")
+    @Test
+    fun shouldBeSelectedColorWhenClickOnItem() {
+        val firstItem = 0
+        Espresso.onView(withId(R.id.recyclerView)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<MainAdapter.MyViewHolder>(
+                firstItem,
+                clickOnViewChild(R.id.eachImage)
+            )
+        )
+        Espresso.onView(withId(R.id.savedPhotos)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.pressBack()
 
+    }
+
+    private fun clickOnViewChild(viewId: Int) = object : ViewAction {
+        override fun getConstraints() = null
+        override fun getDescription() = "Click on a child view with specified id."
+        override fun perform(uiController: UiController, view: View) =
+            ViewActions.click().perform(uiController, view.findViewById<View>(viewId))
+    }
 }
